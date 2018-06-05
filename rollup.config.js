a76c73fa09e2c292eb2069b26d257149aceb93ad
Mark Hayes <mark.hayes@rangle.io>
2018-06-05 16:10:34 -0400

    Build proper modules

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,21 @@
-import babel from 'rollup-plugin-babel';
-
+import babel from "rollup-plugin-babel";
+import json from "rollup-plugin-json";
 export default {
-    input: 'lib/index.js',
-    external: [
-        'joi',
-        'boom'
-    ],
-    plugins: [
-        babel({
-            babelrc: false,
-            exclude: ['./node_modules/**'],
-            presets: [['env', {modules: false, targets: {node: 8}}]]
-        })
-    ],
-    output: [
-        {file: 'dist/index.cjs.js', format: 'cjs', sourcemap: true},
-        {file: 'dist/index.es.js', format: 'es', sourcemap: true}
-    ]
+  input: "lib/index.js",
+  external: ["joi", "boom"],
+  plugins: [
+    babel({
+      babelrc: false,
+      exclude: ["./node_modules/**", "*.json"],
+      presets: [["env", { modules: false, targets: { node: 8 } }]]
+    }),
+    json({
+      preferConst: true,
+      indent: ""
+    })
+  ],
+  output: [
+    { file: "dist/index.cjs.js", format: "cjs", sourcemap: true },
+    { file: "dist/index.es.js", format: "es", sourcemap: true }
+  ]
 };
